700a1d791c5b1673f25d8ca190c92b1e22e980fd
Marcelo <marcelox.alm@>
2023-02-01 21:20:18 -0300

    projetos de Fundamentos de Programação

---
+++ ad1-1/Q1.py
@@ -0,0 +1,53 @@
+### Marcelo da Silva de Almeida
+### Matrícula = 20213050157
+
+"""
+1a Questão (2,0 pontos)
+Faça um programa na Linguagem Python 3 que leia da entrada padrão linhas de texto até
+que uma linha vazia seja digitada. Com exceção da linha vazia, todas as demais possuem
+números inteiros. Seu programa deve escrever na saída padrão a mensagem “Nenhum
+número foi lido – A primeira linha lida foi vazia!!!”, caso a primeira linha lida não contenha
+nenhum número. Caso contrário, escreva o menor número lido, o maior número lido e a
+média dos números lidos, com precisão de duas casas decimais, conforme teste a seguir
+
+1. Ler entradas de texto até que uma linha vazia seja digitada - ok
+2. As entradas precisam ser convertidas em números inteiros após serem lidas - ok
+3. Informar "Nenhum número foi lido – A primeira linha lida foi vazia!!!" caso nada seja informado na primeira linha. - ok
+4. Havendo números, informe o maior, o menor e a média de todos os números com duas casas decimais apenas como resultado.
+
+"""
+## Inicialização de variáveis
+numero = 0
+i=0
+numeros=[]
+soma = 0
+
+## Início do loop while que compara a variável numero com com "" para então começar.
+while numero != "":
+
+    numero = input("Digite um número para iniciar ou tecle enter para terminar")
+
+    ## Ponto de checagem caso nenhum número seja informado. Como essa checagem deve ser feita apenas na primeira vez,
+    ## eu utilizo a variável "i" para fazer o controle da entrada do programa no "if". Eu logo adiciono +1 à "i" para que 
+    ## possa-se sair do programa sem aparecer a imagem abaixo.
+    if numero == "" and i == 0:
+        print("Nenhum número foi lido – A primeira linha lida foi vazia!!!")
+        break
+    i+=1
+    ## Como eu preciso usar a função int() para transformar a variável "numero",
+    ## eu preciso fazer aplicar essa saída "prévia" do laço quando o usuário clicar informar ""
+    ## ou será gerado um erro de "invalid literal for int() with base 10: '' "
+    if numero == "":
+        break
+
+
+    ## Converto e adiciono o valor da variável numero na lista numeros
+    numero = int(numero)
+    numeros.append(numero)
+
+    ## somo numero à soma que foi inicalizada com 0 e 
+    soma = numero + soma
+    i+=1
+
+
+
